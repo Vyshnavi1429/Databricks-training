6e39edde74f5545d72139c9eb1fd85edef8d8041
Vyshnavi1429 <vaishu14296@gmail.com>
2026-05-06 18:21:04 +0530

    Update Queries.sql

--- week 1/Day 2/Queries.sql
+++ week 1/Day 2/Queries.sql
@@ -1,1 +1,10 @@
+--41. Select Employees who are not assigned to any project
+    select e.name
+from Employee e
+left join Department d
+on e.department_id = d.department_id
+left join Project p
+on d.department_id = p.department_id
+where p.project_id is null;
+
 
