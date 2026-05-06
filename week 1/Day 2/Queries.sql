63631f1913ba4e2785101c0a82e5c7ee5edbad54
Vyshnavi1429 <vaishu14296@gmail.com>
2026-05-06 18:39:21 +0530

    Update Queries.sql

--- week 1/Day 2/Queries.sql
+++ week 1/Day 2/Queries.sql
@@ -6,10 +6,16 @@
     left join Project p
     on d.department_id = p.department_id
     where p.project_id is null;
+
 --42. Select employees and the number of projects their department is working on.
     select e.name, count(p.project_id) as project_count from Employee e
     left join Department d on e.department_id = d.department_id
     left join Project p on d.department_id = p.department_id
     group by e.name;
+
+--43 Select the departments that have no employees
+     select d.name from Department d left join Employee e on d.department_id = e.department_id
+     where e.emp_id is null;
+
 
 
