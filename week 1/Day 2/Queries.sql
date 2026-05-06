d09430c397b3f62ca2bd095e9461082bf3da36fb
Vyshnavi1429 <vaishu14296@gmail.com>
2026-05-06 18:35:28 +0530

    Update Queries.sql

--- week 1/Day 2/Queries.sql
+++ week 1/Day 2/Queries.sql
@@ -1,10 +1,15 @@
 --41. Select Employees who are not assigned to any project
     select e.name
-from Employee e
-left join Department d
-on e.department_id = d.department_id
-left join Project p
-on d.department_id = p.department_id
-where p.project_id is null;
+    from Employee e
+    left join Department d
+    on e.department_id = d.department_id
+    left join Project p
+    on d.department_id = p.department_id
+    where p.project_id is null;
+--42. Select employees and the number of projects their department is working on.
+    select e.name, count(p.project_id) as project_count from Employee e
+    left join Department d on e.department_id = d.department_id
+    left join Project p on d.department_id = p.department_id
+    group by e.name;
 
 
